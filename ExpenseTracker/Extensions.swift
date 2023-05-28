df3e9a8c90bf723fe9e5a516b1cc5666c5f3c73c
josepabellana <josep.abe.pu@gmail.com>
2023-05-28 12:49:28 +0200

    Add colours set

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Josep Abellana Puyol on 28.05.23.
+//
+
+import Foundation
+import Swift
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
